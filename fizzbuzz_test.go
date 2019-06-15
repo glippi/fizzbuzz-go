dcd154c8699242a86f522eef57871348a0d0baeb
Gabriele Lippi <gabriele@lippi.net>
2019-06-15 12:26:27 +0200

    Refactor tests name

--- fizzbuzz_test.go
+++ fizzbuzz_test.go
@@ -3,7 +3,7 @@ package main
 import "testing"
 
 func TestDivisibleBy(t *testing.T) {
-	t.Run("return false if the 10 and 3", func(t *testing.T) {
+	t.Run("returns true if the modulo between a and b is not 0", func(t *testing.T) {
 		got := isDivisibleByWithNorest(10, 3)
 		want := false
 
@@ -11,7 +11,7 @@ func TestDivisibleBy(t *testing.T) {
 			t.Errorf("got '%v' want '%v'", got, want)
 		}
 	})
-	t.Run("return false if the 15 and 3", func(t *testing.T) {
+	t.Run("returns true if the modulo between a and b is 0", func(t *testing.T) {
 		got := isDivisibleByWithNorest(15, 3)
 		want := true
 
