204233a4549a5c46a3f8ab628ca8f42a7ee97298
Gabriele Lippi <gabriele@lippi.net>
2019-06-15 11:59:28 +0200

    Create failing test

--- fizzbuzz_test.go
+++ fizzbuzz_test.go
@@ -3,7 +3,7 @@ package main
 import "testing"
 
 func TestFizzBuzz(t *testing.T) {
-	t.Run("returns argument if not divisible by 3 and/or 5", func(t *testing.T) {
+	t.Run("returns 1", func(t *testing.T) {
 		got := FizzBuzz(1)
 		want := "1"
 
@@ -30,6 +30,14 @@ func TestFizzBuzz(t *testing.T) {
 	t.Run("returns 'FizzBuzz' when argument is divisible by 3 and 5", func(t *testing.T) {
 		got := FizzBuzz(15)
 		want := "FizzBuzz"
+
+		if got != want {
+			t.Errorf("got '%s' want '%s'", got, want)
+		}
+	})
+	t.Run("returns 2", func(t *testing.T) {
+		got := FizzBuzz(2)
+		want := "2"
 
 		if got != want {
 			t.Errorf("got '%s' want '%s'", got, want)
